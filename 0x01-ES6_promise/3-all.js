c6e889115fe53b11d35c5dec00d382acafec42ef
MickyBe <bmikiyas@gmail.com>
2022-06-15 14:10:17 +0300

    function fixc

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   Promise.all([
-    createUser, uploadPhoto])
+    createUser(), uploadPhoto()])
     .then((data) => {
       console.log(data[0].body, data[1].firstName, data[2].lastName);
     }).catch(() => console.log('Signup system offline'));
