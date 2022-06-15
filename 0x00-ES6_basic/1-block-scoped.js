755bde09aacd03cdc205a28da77a743837372e8b
MickyBe <bmikiyas@gmail.com>
2022-06-15 10:23:11 +0300

    hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    var task = true;
+    var task2 = false;
   }
+
+  var task = false;
+  var task2 = true;
 
   return [task, task2];
 }
