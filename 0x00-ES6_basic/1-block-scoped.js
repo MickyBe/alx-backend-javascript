5057f887c7a042b037f0562c919f71619de31a70
MickyBe <bmikiyas@gmail.com>
2022-06-15 10:37:21 +0300

    scope histing

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,13 +2,16 @@
 
 export default function taskBlock(trueOrFalse) {
 
+  var task;
+  var task2;
+
   if (trueOrFalse) {
     var task = true;
     var task2 = false;
   }
 
-  var task = false;
-  var task2 = true;
+  task = false;
+  task2 = true;
 
   return [task, task2];
 }
